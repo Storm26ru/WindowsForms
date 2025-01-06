ac3a9872ce17a19b224549c1b82d36ef7b0462ce
Storm <Storm@STORMSSD>
2025-01-06 18:59:47 +0300

    done

--- Clok/MainForm.Designer.cs
+++ Clok/MainForm.Designer.cs
@@ -32,46 +32,241 @@ namespace Clok
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.labelTime = new System.Windows.Forms.Label();
+			this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.toolStripMenuItemTopmost = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemShowControls = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripMenuItemShowDate = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemShowWeekday = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripMenuItemChooseFont = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemCustomFont = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemSystemFont = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemColors = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemBackgroundColor = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemForegroundColor = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripMenuItemLoadOnWindowsStartup = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
 			this.timer = new System.Windows.Forms.Timer(this.components);
-			this.cbShowDate = new System.Windows.Forms.CheckBox();
+			this.checkBoxShowDate = new System.Windows.Forms.CheckBox();
+			this.buttonHideControls = new System.Windows.Forms.Button();
+			this.checkBoxShowWeekday = new System.Windows.Forms.CheckBox();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// labelTime
 			// 
 			this.labelTime.AutoSize = true;
+			this.labelTime.ContextMenuStrip = this.contextMenuStrip;
 			this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelTime.Location = new System.Drawing.Point(12, 9);
 			this.labelTime.Name = "labelTime";
-			this.labelTime.Size = new System.Drawing.Size(210, 51);
+			this.labelTime.Size = new System.Drawing.Size(118, 51);
 			this.labelTime.TabIndex = 0;
-			this.labelTime.Text = "labelTime";
+			this.labelTime.Text = "Time";
+			this.labelTime.DoubleClick += new System.EventHandler(this.labelTime_DoubleClick);
+			// 
+			// contextMenuStrip
+			// 
+			this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemTopmost,
+            this.toolStripMenuItemShowControls,
+            this.toolStripSeparator1,
+            this.toolStripMenuItemShowDate,
+            this.toolStripMenuItemShowWeekday,
+            this.toolStripSeparator2,
+            this.toolStripMenuItemChooseFont,
+            this.toolStripMenuItemColors,
+            this.toolStripSeparator3,
+            this.toolStripMenuItemLoadOnWindowsStartup,
+            this.toolStripSeparator4,
+            this.toolStripMenuItemExit});
+			this.contextMenuStrip.Name = "contextMenuStrip";
+			this.contextMenuStrip.Size = new System.Drawing.Size(210, 204);
+			// 
+			// toolStripMenuItemTopmost
+			// 
+			this.toolStripMenuItemTopmost.CheckOnClick = true;
+			this.toolStripMenuItemTopmost.Name = "toolStripMenuItemTopmost";
+			this.toolStripMenuItemTopmost.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemTopmost.Text = "Topmost";
+			this.toolStripMenuItemTopmost.CheckedChanged += new System.EventHandler(this.toolStripMenuItemTopmost_CheckedChanged);
+			// 
+			// toolStripMenuItemShowControls
+			// 
+			this.toolStripMenuItemShowControls.Checked = true;
+			this.toolStripMenuItemShowControls.CheckOnClick = true;
+			this.toolStripMenuItemShowControls.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.toolStripMenuItemShowControls.Name = "toolStripMenuItemShowControls";
+			this.toolStripMenuItemShowControls.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemShowControls.Text = "Show controls";
+			this.toolStripMenuItemShowControls.CheckedChanged += new System.EventHandler(this.toolStripMenuItemShowControls_CheckedChanged);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(206, 6);
+			// 
+			// toolStripMenuItemShowDate
+			// 
+			this.toolStripMenuItemShowDate.CheckOnClick = true;
+			this.toolStripMenuItemShowDate.Name = "toolStripMenuItemShowDate";
+			this.toolStripMenuItemShowDate.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemShowDate.Text = "Show date";
+			this.toolStripMenuItemShowDate.Click += new System.EventHandler(this.toolStripMenuItemShowDate_Click);
+			// 
+			// toolStripMenuItemShowWeekday
+			// 
+			this.toolStripMenuItemShowWeekday.CheckOnClick = true;
+			this.toolStripMenuItemShowWeekday.Name = "toolStripMenuItemShowWeekday";
+			this.toolStripMenuItemShowWeekday.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemShowWeekday.Text = "Show weekday";
+			this.toolStripMenuItemShowWeekday.Click += new System.EventHandler(this.toolStripMenuItemShowWeekday_Click);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(206, 6);
+			// 
+			// toolStripMenuItemChooseFont
+			// 
+			this.toolStripMenuItemChooseFont.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemCustomFont,
+            this.toolStripMenuItemSystemFont});
+			this.toolStripMenuItemChooseFont.Name = "toolStripMenuItemChooseFont";
+			this.toolStripMenuItemChooseFont.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemChooseFont.Text = "Choose font";
+			// 
+			// toolStripMenuItemCustomFont
+			// 
+			this.toolStripMenuItemCustomFont.Name = "toolStripMenuItemCustomFont";
+			this.toolStripMenuItemCustomFont.Size = new System.Drawing.Size(141, 22);
+			this.toolStripMenuItemCustomFont.Text = "Custom font";
+			this.toolStripMenuItemCustomFont.Click += new System.EventHandler(this.toolStripMenuItemCustomFont_Click);
+			// 
+			// toolStripMenuItemSystemFont
+			// 
+			this.toolStripMenuItemSystemFont.Name = "toolStripMenuItemSystemFont";
+			this.toolStripMenuItemSystemFont.Size = new System.Drawing.Size(141, 22);
+			this.toolStripMenuItemSystemFont.Text = "System font";
+			this.toolStripMenuItemSystemFont.Click += new System.EventHandler(this.toolStripMenuItemSystemFont_Click);
+			// 
+			// toolStripMenuItemColors
+			// 
+			this.toolStripMenuItemColors.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemBackgroundColor,
+            this.toolStripMenuItemForegroundColor});
+			this.toolStripMenuItemColors.Name = "toolStripMenuItemColors";
+			this.toolStripMenuItemColors.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemColors.Text = "Colors";
+			// 
+			// toolStripMenuItemBackgroundColor
+			// 
+			this.toolStripMenuItemBackgroundColor.Name = "toolStripMenuItemBackgroundColor";
+			this.toolStripMenuItemBackgroundColor.Size = new System.Drawing.Size(168, 22);
+			this.toolStripMenuItemBackgroundColor.Text = "Background color";
+			this.toolStripMenuItemBackgroundColor.Click += new System.EventHandler(this.toolStripMenuItemBackgroundColor_Click);
+			// 
+			// toolStripMenuItemForegroundColor
+			// 
+			this.toolStripMenuItemForegroundColor.Name = "toolStripMenuItemForegroundColor";
+			this.toolStripMenuItemForegroundColor.Size = new System.Drawing.Size(168, 22);
+			this.toolStripMenuItemForegroundColor.Text = "Foreground color";
+			this.toolStripMenuItemForegroundColor.Click += new System.EventHandler(this.toolStripMenuItemBackgroundColor_Click);
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(206, 6);
+			// 
+			// toolStripMenuItemLoadOnWindowsStartup
+			// 
+			this.toolStripMenuItemLoadOnWindowsStartup.CheckOnClick = true;
+			this.toolStripMenuItemLoadOnWindowsStartup.Name = "toolStripMenuItemLoadOnWindowsStartup";
+			this.toolStripMenuItemLoadOnWindowsStartup.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemLoadOnWindowsStartup.Text = "Load on Windows startup";
+			this.toolStripMenuItemLoadOnWindowsStartup.Click += new System.EventHandler(this.toolStripMenuItemLoadOnWindowsStartup_Click);
+			// 
+			// toolStripSeparator4
+			// 
+			this.toolStripSeparator4.Name = "toolStripSeparator4";
+			this.toolStripSeparator4.Size = new System.Drawing.Size(206, 6);
+			// 
+			// toolStripMenuItemExit
+			// 
+			this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+			this.toolStripMenuItemExit.Size = new System.Drawing.Size(209, 22);
+			this.toolStripMenuItemExit.Text = "Exit";
+			this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
 			// 
 			// timer
 			// 
 			this.timer.Enabled = true;
 			this.timer.Tick += new System.EventHandler(this.timer_Tick);
 			// 
-			// cbShowDate
+			// checkBoxShowDate
 			// 
-			this.cbShowDate.AutoSize = true;
-			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cbShowDate.Location = new System.Drawing.Point(12, 163);
-			this.cbShowDate.Name = "cbShowDate";
-			this.cbShowDate.Size = new System.Drawing.Size(176, 29);
-			this.cbShowDate.TabIndex = 1;
-			this.cbShowDate.Text = "Показать дату";
-			this.cbShowDate.UseVisualStyleBackColor = true;
+			this.checkBoxShowDate.AutoSize = true;
+			this.checkBoxShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.checkBoxShowDate.Location = new System.Drawing.Point(12, 163);
+			this.checkBoxShowDate.Name = "checkBoxShowDate";
+			this.checkBoxShowDate.Size = new System.Drawing.Size(132, 29);
+			this.checkBoxShowDate.TabIndex = 1;
+			this.checkBoxShowDate.Text = "Show date";
+			this.checkBoxShowDate.UseVisualStyleBackColor = true;
+			this.checkBoxShowDate.CheckedChanged += new System.EventHandler(this.checkBoxShowDate_CheckedChanged);
+			// 
+			// buttonHideControls
+			// 
+			this.buttonHideControls.BackColor = System.Drawing.SystemColors.Control;
+			this.buttonHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.buttonHideControls.Location = new System.Drawing.Point(11, 243);
+			this.buttonHideControls.Name = "buttonHideControls";
+			this.buttonHideControls.Size = new System.Drawing.Size(176, 44);
+			this.buttonHideControls.TabIndex = 2;
+			this.buttonHideControls.Text = "Hide controls";
+			this.buttonHideControls.UseVisualStyleBackColor = false;
+			this.buttonHideControls.Click += new System.EventHandler(this.buttonHideControls_Click);
+			// 
+			// checkBoxShowWeekday
+			// 
+			this.checkBoxShowWeekday.AutoSize = true;
+			this.checkBoxShowWeekday.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.checkBoxShowWeekday.Location = new System.Drawing.Point(12, 198);
+			this.checkBoxShowWeekday.Name = "checkBoxShowWeekday";
+			this.checkBoxShowWeekday.Size = new System.Drawing.Size(175, 29);
+			this.checkBoxShowWeekday.TabIndex = 3;
+			this.checkBoxShowWeekday.Text = "Show weekday";
+			this.checkBoxShowWeekday.UseVisualStyleBackColor = true;
+			this.checkBoxShowWeekday.CheckedChanged += new System.EventHandler(this.checkBoxShowWeekday_CheckedChanged);
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "notifyIcon1";
+			this.notifyIcon.Visible = true;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.cbShowDate);
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(278, 298);
+			this.Controls.Add(this.checkBoxShowWeekday);
+			this.Controls.Add(this.buttonHideControls);
+			this.Controls.Add(this.checkBoxShowDate);
 			this.Controls.Add(this.labelTime);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
-			this.Text = "ClockPV_311";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Clock VPD_311";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+			this.contextMenuStrip.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -81,7 +276,27 @@ namespace Clok
 
 		private System.Windows.Forms.Label labelTime;
 		private System.Windows.Forms.Timer timer;
-		private System.Windows.Forms.CheckBox cbShowDate;
+		private System.Windows.Forms.CheckBox checkBoxShowDate;
+		private System.Windows.Forms.Button buttonHideControls;
+		private System.Windows.Forms.CheckBox checkBoxShowWeekday;
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemTopmost;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemShowControls;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemShowDate;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemShowWeekday;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemChooseFont;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemColors;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemBackgroundColor;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemForegroundColor;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemLoadOnWindowsStartup;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemCustomFont;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSystemFont;
 	}
 }
 
--- Clok/MainForm.cs
+++ Clok/MainForm.cs
@@ -7,24 +7,197 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Text;
+using Microsoft.Win32;
+using System.IO;
+
 
 namespace Clok
 {
 	public partial class MainForm : Form
 	{
+		Uri uriFileFont;
+		PrivateFontCollection privateFont = new PrivateFontCollection();
+		bool CustomFont;
 		public MainForm()
 		{
 			InitializeComponent();
+			labelTime.BackColor = Color.AliceBlue;
+			this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, 50);
+			RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+			if (key.GetValue("Clok_VPD_311") != null) toolStripMenuItemLoadOnWindowsStartup.Checked = true;
+			key.Dispose();
+			LoadValue();
+			
+		}
+		void SaveValue()
+		{
+			RegistryKey key = Registry.CurrentUser.CreateSubKey("Software\\Clock_VPD_311");
+			key.SetValue("Topmost", toolStripMenuItemTopmost.Checked);
+			key.SetValue("Show_controls", toolStripMenuItemShowControls.Checked);
+			key.SetValue("Show_date",toolStripMenuItemShowDate.Checked);
+			key.SetValue("Show_weekday",toolStripMenuItemShowWeekday.Checked);
+			if(CustomFont) key.SetValue("Font",uriFileFont.AbsolutePath);
+			else key.SetValue("Font",labelTime.Font.Name);
+			key.SetValue("Backgraund_color",labelTime.BackColor.ToArgb());
+			key.SetValue("Foregraund_color",labelTime.ForeColor.ToArgb());
+			key.Close();
+			key.Dispose();
+		}
+		void LoadValue()
+		{
+			RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Clock_VPD_311", false);
+			toolStripMenuItemTopmost.Checked = Convert.ToBoolean(key.GetValue("Topmost"));
+			toolStripMenuItemShowControls.Checked = Convert.ToBoolean(key.GetValue("Show_controls"));
+			checkBoxShowDate.Checked = Convert.ToBoolean(key.GetValue("Show_date"));
+			checkBoxShowWeekday.Checked = Convert.ToBoolean(key.GetValue("Show_weekday"));
+			if (Convert.ToString(key.GetValue("Font")).Contains('/'))
+			{
+				privateFont.AddFontFile(Convert.ToString(key.GetValue("Font")));
+				labelTime.Font = new Font(privateFont.Families[0], labelTime.Font.SizeInPoints);
+			}
+			else labelTime.Font = new Font(new FontFamily(Convert.ToString(key.GetValue("Font"))), labelTime.Font.SizeInPoints);
+			labelTime.BackColor = Color.FromArgb(Convert.ToInt32(key.GetValue("Backgraund_color")));
+			labelTime.ForeColor = Color.FromArgb(Convert.ToInt32(key.GetValue("Foregraund_color")));
+			key.Dispose();
+		}
+		void SetVisibility(bool visible)
+		{
+			checkBoxShowDate.Visible = visible;
+			checkBoxShowWeekday.Visible = visible;
+			buttonHideControls.Visible = visible;
+			this.FormBorderStyle = visible ? FormBorderStyle.FixedDialog : FormBorderStyle.None;
+			this.ShowInTaskbar = visible;
+			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
 		}
 
 		private void timer_Tick(object sender, EventArgs e)
 		{
-			labelTime.Text = DateTime.Now.ToString("H:mm:ss tt",System.Globalization.CultureInfo.InvariantCulture);
-			if (cbShowDate.Checked)
+			labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt",System.Globalization.CultureInfo.InvariantCulture);
+			if (checkBoxShowDate.Checked)
+				labelTime.Text += $"\n{DateTime.Now.ToString("dd.MM.yyyy")}";
+			if (checkBoxShowWeekday.Checked) 
+				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+			notifyIcon.Text =	$"{DateTime.Now.ToString("H: mm")}\n"+
+								$"{DateTime.Now.ToString("dd.MM.yyyy")}\n"+
+								$"{DateTime.Now.DayOfWeek}";
+		}
+
+		private void buttonHideControls_Click(object sender, EventArgs e)
+		{
+			SetVisibility(toolStripMenuItemShowControls.Checked = false);
+		
+		}
+
+		private void labelTime_DoubleClick(object sender, EventArgs e)
+		{
+			SetVisibility(toolStripMenuItemShowControls.Checked = true);
+		}
+
+		private void toolStripMenuItemTopmost_CheckedChanged(object sender, EventArgs e)
+		{
+			this.TopMost = toolStripMenuItemTopmost.Checked;
+		}
+
+		private void toolStripMenuItemExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+			privateFont.Dispose();
+		}
+				
+		private void toolStripMenuItemShowControls_CheckedChanged(object sender, EventArgs e)
+		{
+			SetVisibility(toolStripMenuItemShowControls.Checked);
+		}
+		private void toolStripMenuItemShowDate_Click(object sender, EventArgs e)
+		{
+			checkBoxShowDate.Checked = toolStripMenuItemShowDate.Checked;
+		}
+
+		private void checkBoxShowDate_CheckedChanged(object sender, EventArgs e)
+		{
+			toolStripMenuItemShowDate.Checked = checkBoxShowDate.Checked;
+		}
+
+		private void toolStripMenuItemShowWeekday_Click(object sender, EventArgs e)
+		{
+			checkBoxShowWeekday.Checked = toolStripMenuItemShowWeekday.Checked;
+		}
+
+		private void checkBoxShowWeekday_CheckedChanged(object sender, EventArgs e)
+		{
+			toolStripMenuItemShowWeekday.Checked = checkBoxShowWeekday.Checked;
+		}
+
+		private void toolStripMenuItemBackgroundColor_Click(object sender, EventArgs e)
+		{
+			ColorDialog colorDialog = new ColorDialog();
+			colorDialog.AllowFullOpen = true;
+			if(sender.ToString()=="Background color")
 			{
-				labelTime.Text += "\n";
-				labelTime.Text += DateTime.Now.ToString("dd.MM.yyyy");
-			} 
+				colorDialog.Color = labelTime.BackColor;
+				if (colorDialog.ShowDialog() == DialogResult.OK)
+					labelTime.BackColor = colorDialog.Color;
+			}else
+			{
+				colorDialog.Color = labelTime.ForeColor;
+				if (colorDialog.ShowDialog() == DialogResult.OK)
+					labelTime.ForeColor = colorDialog.Color;
+			}
+		}
+
+		private void toolStripMenuItemLoadOnWindowsStartup_Click(object sender, EventArgs e)
+		{
+			RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run",true);
+			if (toolStripMenuItemLoadOnWindowsStartup.Checked)
+				key.SetValue("Clok_VPD_311", Application.ExecutablePath);
+			else
+				key.DeleteValue("Clok_VPD_311");
+			key.Close();
+
+		}
+
+		private void toolStripMenuItemCustomFont_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFile = new OpenFileDialog()
+			{
+				FileName = "Select a font file",
+				Filter = "Font files |*.ttf;*.otf",
+				Title = "Open font file"
+			};
+			if(openFile.ShowDialog()==DialogResult.OK)
+			{
+				uriFileFont = new Uri(openFile.FileName);
+				PrivateFontCollection fontbufer = new PrivateFontCollection();
+				privateFont.AddFontFile(uriFileFont.AbsolutePath);
+				fontbufer.AddFontFile(uriFileFont.AbsolutePath);
+				for (int i = 0; i <privateFont.Families.Length; i++)
+				{
+					if (privateFont.Families[i].Name == fontbufer.Families[0].Name)
+					{
+						labelTime.Font = new Font(privateFont.Families[i], labelTime.Font.SizeInPoints);
+						CustomFont = true;
+						break;
+					}
+				}
+				fontbufer.Dispose();
+			}
+
+		}
+
+		private void toolStripMenuItemSystemFont_Click(object sender, EventArgs e)
+		{
+			FontDialog fontDialog = new FontDialog();
+			if(fontDialog.ShowDialog()==DialogResult.OK)
+			{
+				labelTime.Font = fontDialog.Font;
+				CustomFont = false;
+			}
+		}
+
+		private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			SaveValue();
 		}
 	}
 }
